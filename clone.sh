49e2d0048610a71c410b616cb54960665c89ad65
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-09-09 17:40:49 +0200

    Updated clone script to do some further magic.

--- clone.sh
+++ clone.sh
@@ -4,5 +4,8 @@ if [ -z "$SSH_AUTH_SOCK" ] ; then
   eval `ssh-agent -s`
   ssh-add
 fi
-
+# Clone all repositories recursively
 git clone --recursive git@github.com:Skeen/MetaThesis.git
+# Checkout branches
+git submodule foreach -q --recursive \
+  'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
